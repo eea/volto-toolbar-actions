a364297fbdbe9c5ad6653c592d462fd2147253c8
Alin Voinea <contact@avoinea.com>
2023-09-22 19:04:16 +0300

    feat: Display actions from toolbar_actions category in left Toolbar - refs #258073

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+import { default as ToolbarActions } from './components/ToolbarActions';
+
 const applyConfig = (config) => {
+  config.settings.appExtras = [
+    ...(config.settings.appExtras || []),
+    {
+      match: '',
+      component: ToolbarActions,
+    },
+  ];
   return config;
 };
 
