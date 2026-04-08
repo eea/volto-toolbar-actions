3aac9b0ae51437ca2bf2a979d3b4fd63db404447
Alin Voinea <contact@avoinea.com>
2026-04-08 16:19:51 +0300

    chore: Volto 18 eslint/jest migration - refs #301881 (#13)
    * chore: Upgrade eslint to Volto 18
    
    * chore: Volto 18 eslint/jest migration - refs #301881 (volto-toolbar-actions)

--- src/components/ToolbarAction.jsx
+++ src/components/ToolbarAction.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'semantic-ui-react';
-import { UniversalLink } from '@plone/volto/components';
+import UniversalLink from '@plone/volto/components/manage/UniversalLink/UniversalLink';
 
 const ToolbarAction = (props) => {
   const { item } = props;
